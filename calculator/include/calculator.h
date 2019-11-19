53b7a46ed185effe9d02dc632d1fa9b7f8f4d6e2
Pablo Diego <pablodiegoss@hotmail.com>
2019-11-19 11:56:22 -0300

    Push cbindgen files

---
+++ calculator/include/calculator.h
@@ -0,0 +1,14 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+
+
+struct String;
+
+extern "C" {
+
+String rodrigo_lixo(int32_t a, int32_t b);
+
+} // extern "C"
+
---
+++ calculator/src/lib.rs
@@ -0,0 +1,5 @@
+
+#[no_mangle] 
+pub extern fn rodrigo_lixo(a:i32, b:i32)-> String{
+    return format!("{}{}",a,b);
+}
