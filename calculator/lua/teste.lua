4b1b59fc43c9d548e1deb8b7e7c6e23735e5ef9a
Pablo Diego <pablodiegoss@hotmail.com>
2019-11-27 10:50:47 -0300

    Delete old rust projects and make mock-engine be the main one

--- calculator/lua/teste.lua
+++
@@ -1,5 +0,0 @@
-package.cpath = "./target/debug/libcalculator.so"
-require "fromlua"
-print ("hello")
-print (rodrigo_lixo(15,10))
-print ("It's run da rust code!")
--- mock-engine/shared/target/debug/libshared.d
+++
@@ -1,1 +0,0 @@
-/home/shammyz/Documents/repos/stainless-experiments/mock-engine/shared/target/debug/libshared.so: /home/shammyz/Documents/repos/stainless-experiments/mock-engine/shared/src/lib.rs
